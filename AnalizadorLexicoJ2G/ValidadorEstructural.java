3b4e84294e5f04268df3776f66720958959a5077
Yourchh <hu220111169@lahuerta.tecmm.edu.mx>
2025-05-15 22:40:45 -0600

    Remove unused import statements in ValidadorEstructural.java

--- AnalizadorLexicoJ2G/ValidadorEstructural.java
+++ AnalizadorLexicoJ2G/ValidadorEstructural.java
@@ -1,10 +1,6 @@
 package AnalizadorLexicoJ2G;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
